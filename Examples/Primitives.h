12413df9f0ebf550a3f685f1ab127ce670fe1fa7
Andrew Nicoara <ldnic2013@gmail.com>
2018-03-29 22:11:44 -0500

    Update Primitives.h

--- Examples/Primitives.h
+++ Examples/Primitives.h
@@ -1,1 +1,15 @@
 #pragma once
+
+//Wrapper Classes
+#include "VertexArray.h"
+#include "Buffer.h"
+#include "Program.h"
+
+//External Libraries
+#include <glm.hpp>
+#include <gtc\matrix_transform.hpp>
+#include <gtc\type_ptr.hpp>
+
+class Primitive
+{
+};
